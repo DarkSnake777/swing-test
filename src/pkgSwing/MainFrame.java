8d35a921b1b7bbb4e6a07069deb95dcca51f8fda
Vitaly Chernikov <ds84@mail.ru>
2017-04-13 12:22:13 +0300

    add (c)

--- src/pkgSwing/MainFrame.java
+++ src/pkgSwing/MainFrame.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2010-2017 Chernikov Vitaly. All rights reserved.
+ */
+
 package pkgSwing;
 //new comment
 
@@ -25,10 +29,7 @@ import javax.swing.tree.TreeModel;
 
 
 
-/**
- * @author Vitaly
- *
- */
+
 class MainFrame extends JFrame {
 	
 	
