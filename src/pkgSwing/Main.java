171840e485424b03cf6a01048686ac7ad22c6905
Vitaly Chernikov <ds84@mail.ru>
2017-04-13 16:54:51 +0300

    some comments

--- src/pkgSwing/Main.java
+++ src/pkgSwing/Main.java
@@ -15,6 +15,7 @@ class Main {
 	
 	public static void main(String[] args)
 	{
+		//entry point
 		SwingUtilities.invokeLater(MainFrame::new);
 		 System.out.println("This might well be displayed before the other message.");
 
