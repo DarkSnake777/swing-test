cca2c6a4ff64fefe59aa5186f7f279293b229604
Vitaly Chernikov <ds84@mail.ru>
2017-04-13 17:24:11 +0300

    some master changes

--- src/pkgSwing/Main.java
+++ src/pkgSwing/Main.java
@@ -15,6 +15,7 @@ class Main {
 	
 	public static void main(String[] args)
 	{
+		//some master changes
 		//entry point
 		SwingUtilities.invokeLater(MainFrame::new);
 		 System.out.println("This might well be displayed before the other message.");
